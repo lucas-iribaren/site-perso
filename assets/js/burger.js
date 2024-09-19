1e9aff1b59c3531ccac95a4cb98a82005dde0b89
lucas-iribaren <lucas.iribaren@laplateforme.io>
2024-09-19 16:18:15 +0200

    Add burger.js for toggling navigation menu

---
+++ assets/js/burger.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', function() {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+});
